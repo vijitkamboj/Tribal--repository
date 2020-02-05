33253921588548a8330d85226aeb1855ac8c5928
Vijit Kamboj <vijitkamboj92@gmail.com>
2020-02-05 23:13:38 +0530

    navbar

---
+++ src/Containers/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class FileModal extends Component {
+    state = {
+        file: null,
+        isFormEmpty: true,
+        authorized: ["image/jpeg", "image/png"]
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+export default FileModal
