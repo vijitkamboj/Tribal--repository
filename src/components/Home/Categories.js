ca636429399919ce5ce842935558f2263fd18980
Vijit Kamboj <vijitkamboj92@gmail.com>
2020-02-06 10:44:14 +0530

    added dropsown

--- src/components/Home/Categories.js
+++ src/components/Home/Categories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
-import {Link} from 'react-router-dom';
+
+
 const Categories = () => {
     return(
         <div id="categories">
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import './Nav.css';
 import {Link} from 'react-router-dom';
 import AboutModal from "./AboutModal";
+import {Dropdown} from "semantic-ui-react"
 
 
 class Nav extends Component {
@@ -20,6 +21,34 @@ class Nav extends Component {
             modal: false
         })
     }
+    dropDownOptions =()=>{
+    
+        return(
+          [
+              {   
+                  key:"category",
+                  text:<a href="#photos"><span>Photos</span></a>,
+              },
+              {   
+                key:"category",
+                text:<a href="#videos"><span>Videos</span></a>,
+            },
+            {   
+                key:"category",
+                text:<a href="#audio"><span>Music</span></a>,
+            },
+            {   
+                key:"category",
+                text:<a href="#documents"><span>Documents</span></a>,
+            },
+            {   
+                key:"category",
+                text:<a href="#photos"><span>Sports</span></a>,
+            }
+          ]
+        )
+      }
+      
     render(){
         return(
             <div id="nav">
@@ -28,7 +57,14 @@ class Nav extends Component {
             <div className="navbtn"><Link to="/home" className='links'>Home</Link></div>
             <div className="navbtn" onClick={this.openModal}><Link to="/home#about" className='links' >About</Link></div>
             <div className="navbtn"><Link to="/home" className='links'>Contact Us</Link></div>
-            <div className="navbtn"><Link to="/home" className='links'>Categories</Link></div>
+            <Dropdown 
+                id="user-panel-dropdown" 
+                trigger={
+                    <div className="navbtn"><Link to="/home" className='links'>Categories</Link></div>
+                } 
+                options={this.dropDownOptions()}
+                
+                />
             {/* <div className="navbtn"><Link to="/home" className='links'>Dashboard</Link></div> */}
             <div className="navbtn"><Link to="/login" className='links'>Login</Link></div>
             <AboutModal modal={this.state.modal} closeModal={this.closeModal}/>
